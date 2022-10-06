a8490cd17650d240b0048acb0b756be19a718c11
Gustavo Galo <gg.gustavo.galo@gmail.com>
2022-10-06 20:58:17 -0300

    refactor: change component directorty

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,11 @@
 import type { NextPage } from "next";
-import LoginForm from "../../components/login-form";
+import LoginForm from "../components/login-form";
 
 const Home: NextPage = () => {
   return (
-    <div>
-      <head>
-        <title>Login Page</title>
-        <link rel="icon" href="/favicon.ico" />
-      </head>
-      <main>
-           <LoginForm/>
-      </main>
-    </div>
+    <main>
+      <LoginForm />
+    </main>
   );
 };
 
