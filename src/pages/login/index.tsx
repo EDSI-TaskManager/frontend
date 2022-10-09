ce5f995e820d1c0d458ae76c4500962ecb880dd1
Gustavo Galo <gg.gustavo.galo@gmail.com>
2022-10-09 14:07:47 -0300

    feat: login page

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,3 +1,47 @@
-const Login = () => <div>Login page works!</div>;
+import { useState } from "react";
+import { TextField, Background } from "../../components/";
+
+const Login = () => {
+  const [emailOrUsername, setEmailOrUsername] = useState("");
+  const [passoword, setPassoword] = useState("");
+
+  return (
+    <Background>
+      <div className="block h-screen sm:flex sm:justify-center sm:items-center">
+        <div className="h-full bg-gray100  flex flex-col justify-center sm:w-[26rem] p-8 sm:rounded-lg sm:h-auto">
+          <div className="flex flex-col gap-4">
+            <h1 className="text-center text-white text-2xl font-bold mb-10">
+              Boas vindas!
+            </h1>
+            <TextField
+              label="E-MAIL OU NOME DE USUÁRIO"
+              value={emailOrUsername}
+              setValue={setEmailOrUsername}
+            />
+            <TextField
+              label="SENHA"
+              value={passoword}
+              setValue={setPassoword}
+            />
+            <a href="" className="text-primary">
+              Esqueceu sua senha?
+            </a>
+            <button className="bg-primary border-none rounded p-2 text-white font-bold">
+              Entar
+            </button>
+            <div>
+              <span className="text-sm font-bold">
+                Precisando de uma conta?{" "}
+              </span>
+              <a href="" className="text-sm font-bold text-primary">
+                Registre-se
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </Background>
+  );
+};
 
 export default Login;
