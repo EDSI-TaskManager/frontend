eeba0289f245533d6c717648c9cc95d8a55e05f2
Miguel Soares <miguelstrive@gmail.com>
2022-10-20 09:01:14 -0300

    feat: add prop type at TextField component

--- src/components/TextField/index.tsx
+++ src/components/TextField/index.tsx
@@ -4,15 +4,22 @@ interface Props extends HTMLAttributes<HTMLInputElement> {
   label: string;
   value: string;
   setValue: Dispatch<SetStateAction<string>>;
+  type?: "text" | "password";
 }
 
-export const TextField = ({ label, value, setValue, ...restProps }: Props) => {
+export const TextField = ({
+  label,
+  value,
+  setValue,
+  type = "text",
+  ...restProps
+}: Props) => {
   return (
     <div>
       <p className="font-semibold mb-1">{label}</p>
       <input
         {...restProps}
-        type="text"
+        type={type}
         value={value}
         onChange={(e) => setValue(e.target.value)}
         className="bg-gray400 text-white border-black border outline-0 rounded p-1 w-full"
