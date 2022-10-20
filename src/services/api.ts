64c3f6ffcc2f765d134f52a9adc8a881cbe48e24
Gustavo Galo <gg.gustavo.galo@gmail.com>
2022-10-20 17:59:43 -0300

    fix: api service

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,35 +1,9 @@
-import axios, { AxiosInstance } from "axios";
+import axios from "axios";
 
-export class ApiService {
-  private static _instance = new ApiService();
-  private token = "";
-  private static _api: AxiosInstance;
+export const api = axios.create({
+  baseURL: process.env.NEXT_PUBLIC_API_BASE_URL,
+});
 
-  private constructor() {
-    ApiService._api = axios.create({
-      baseURL: process.env.NEXT_PUBLIC_API_BASE_URL,
-    });
-  }
-
-  static get instance() {
-    if (!ApiService.instance) {
-      ApiService._instance = new ApiService();
-    }
-
-    return this._instance;
-  }
-
-  static get api() {
-    return this._api;
-  }
-
-  static set token(token: string) {
-    this.token = token;
-    ApiService._api = axios.create({
-      baseURL: process.env.NEXT_PUBLIC_API_BASE_URL,
-      headers: {
-        Authorization: "Bearer " + token,
-      },
-    });
-  }
-}
+export const setApiToken = (token: string) => {
+  axios.defaults.headers.common["Authorization"] = `Bearer ${token}`;
+};
