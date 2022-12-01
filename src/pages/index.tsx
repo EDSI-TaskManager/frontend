3889e420b6db65e0e060e999fc98180dcf599a5a
Gustavo Galo <gg.gustavo.galo@gmail.com>
2022-12-01 15:06:53 -0300

    fix: register

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,9 +2,6 @@ import type { GetServerSideProps, NextPage } from "next";
 
 const Home: NextPage = () => null;
 
-/**
- * TODO: if already logged, redirect to another page
- */
 export const getServerSideProps: GetServerSideProps = async () => {
   return {
     redirect: { destination: "/login", permanent: false },
